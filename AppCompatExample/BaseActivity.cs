0b307612ca8c3e2800170fbb7d73856dccbc4a40
Michal Orzechowski <morzechowski1@pl.sii.eu>
2017-05-16 19:25:48 +0200

    minimum AppCompat with toolbar

---
+++ AppCompatExample/BaseActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+
+namespace AppCompatExample
+{
+	public abstract class BaseActivity : AppCompatActivity
+	{
+		public Toolbar Toolbar { get; set; }
+		
+		protected abstract int LayoutResource { get; }
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(LayoutResource);
+			Toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+
+			if (Toolbar != null)
+			{
+				SetSupportActionBar(Toolbar);
+			}
+		}
+	}
+}
--- AppCompatExample/MainActivity.cs
+++ AppCompatExample/MainActivity.cs
@@ -1,18 +1,18 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace AppCompatExample
 {
 	[Activity(Label = "AppCompatExample", MainLauncher = true)]
-	public class MainActivity : Activity
+	public class MainActivity : BaseActivity
 	{
+		protected override int LayoutResource => Resource.Layout.Main;
+
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
-
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
 		}
 	}
 }
